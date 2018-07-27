54349de81a009b80519792e41bd67909571fced7
aswinimuttineni <41729935+aswinimuttineni@users.noreply.github.com>
2018-07-27 15:35:35 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			int num;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+	         int a=1;
+	         int b=0;
+	    while(a<=num)
+	    {
+	    	if((num%a)==0)
+	    	b=b+1;
+	    	a++;
+	    }
+	    if(b==2)
+	    {
+	    	System.out.println("yes");
+	    }
+	    else
+	    {
+	    	System.out.println("no");
+	    }
+	    
+	    
+	
+	}
+}
