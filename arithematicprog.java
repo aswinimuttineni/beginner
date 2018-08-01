5db87623ade2f23361131b75a9b43dc32fbd04cd
aswinimuttineni <41729935+aswinimuttineni@users.noreply.github.com>
2018-08-01 14:43:29 +0530

    Create arithematicprog.java

---
+++ arithematicprog.java
@@ -0,0 +1,23 @@
+
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int N,A,D;
+		Scanner sc=new Scanner(System.in);
+		N=sc.nextInt();
+		A=sc.nextInt();
+		D=sc.nextInt();
+		System.out.println(N*(2*A+(N-1)*D)/2);
+		
+		
+	}
+}
