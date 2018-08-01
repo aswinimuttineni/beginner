bacdc7fd047b3d9443ff80845b8e193a8bb90511
aswinimuttineni <41729935+aswinimuttineni@users.noreply.github.com>
2018-08-01 15:14:26 +0530

    Create largerarray.java

---
+++ largerarray.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int num,max;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		int a[]=new int[num];
+		for(int i=0;i<num;i++)
+		{
+			a[i]=sc.nextInt();
+			
+		}
+		max=a[0];
+		for(int i=0;i<num;i++)
+		{
+			if(max<a[i])
+			{
+				max=a[i];
+			}
+		}
+		System.out.println(max);
+	}
+}
