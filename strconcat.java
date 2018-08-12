5a4cdebea690d7658cc5e39154d38f3772f6324d
aswinimuttineni <41729935+aswinimuttineni@users.noreply.github.com>
2018-08-12 12:54:34 +0530

    Create strconcat.java

---
+++ strconcat.java
@@ -0,0 +1,23 @@
+
+		/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str1,str2;
+		String str3;
+		Scanner sc=new Scanner(System.in);
+		str1=sc.next();
+		str2=sc.next();
+		str3=str1+str2;
+		System.out.println(str3);
+	}
+}
+	
