0ba838632fc8bdae4ef6437b27c0315f8346b692
aswinimuttineni <41729935+aswinimuttineni@users.noreply.github.com>
2018-08-12 15:19:07 +0530

    Create avgnum.java

---
+++ avgnum.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,sum=0;
+		float avg;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int[] a=new int[10];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+a[i];
+		}
+		avg=(float)sum/n;
+		System.out.println(avg);
+	}
+}
