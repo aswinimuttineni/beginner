3cf8755219b053d24f45824eb4ca4eda77c56d03
aswinimuttineni <41729935+aswinimuttineni@users.noreply.github.com>
2018-08-28 10:51:10 +0530

    Update decimal82.java

--- ash/decimal82.java
+++ ash/decimal82.java
@@ -10,12 +10,11 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		float l;
-		float b;
+		double l,b,a;
 		Scanner sc=new Scanner(System.in);
-		l=sc.nextFloat();
-		b=sc.nextFloat();
-		float a=l*b;
+		l=sc.nextDouble();
+		b=sc.nextDouble();
+		 a=l*b;
 		System.out.printf("%3.5f",a);
 	}
 }
