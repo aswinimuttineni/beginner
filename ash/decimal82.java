adead6f2748e347cd7f5164e53b4e7203dfd903a
aswinimuttineni <41729935+aswinimuttineni@users.noreply.github.com>
2018-08-28 10:41:54 +0530

    Create decimal82.java

---
+++ ash/decimal82.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		float l;
+		float b;
+		Scanner sc=new Scanner(System.in);
+		l=sc.nextFloat();
+		b=sc.nextFloat();
+		float a=l*b;
+		System.out.printf("%3.5f",a);
+	}
+}
